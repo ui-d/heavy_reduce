d11cf94e5ae2998f365f1c1bd8b4ba4c2acc7e86
ui-d <dawiddeveloper@gmail.com>
2021-11-28 20:20:12 +0100

    arrays concatenation with reduce

--- reduce.js
+++ reduce.js
@@ -1,7 +1,36 @@
 // sum arr values
 
 let arr = [1, 2, 3, 44, 45, 75, 77];
+
 let sum = arr.reduce((sum, acc) => {
     return sum + acc;
-}, 0); // can omit
+}, 0); // can omit second argument
+
+// concat arr values
+
+let cars = [
+     {
+        model: ["A4", "A6", "A8"],
+        year: '2016'
+    },
+    {
+        model: ["X5", "X6", "X7"],
+        year: '2017'
+    },
+     {
+        model: ["C", "E", "S"],
+        year: '2018'
+    }
+]
+
+let concatedArrays = cars.reduce((acc, val) => {
+    return acc.concat(val.model);
+}, []);
+
+concatedArrays
+
+
+
+
+
 
